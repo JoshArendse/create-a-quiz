99302b604c655ab5211f7490bfb12bfcffc6195c
JoshArendse <42963960+JoshArendse@users.noreply.github.com>
2018-11-15 12:55:30 +0200

    Quiz Implimented
    Quiz Stored as a php extension, styling and about 18 questions added, base layout and styling is done with Bulma Framework

---
+++ index.php
@@ -0,0 +1,562 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bulma.min.css">
+    <title>Create A Quiz</title>
+</head>
+<body>
+     
+    <div class="container is-fluid">
+    <div class="notification">
+
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Which Ninja Are You?</h1>
+                    <h2 class="subtitle">
+                        Answer these simple questions to find out which ninja you'd most likely be if you had chakra!
+                    </h2>
+            </div>
+        </section>
+
+    </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 1</h1>
+                <h2 class="subtitle">Which style do you prefer?</h2>
+               
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fire Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Earth Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Wind Style
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 2</h1>
+                <h2 class="subtitle">What is your favourite food?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Anything Spicy
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    I don't Eat Much
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Healthy Foods Rich In vegetables and lean meat
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Ramen Noodles
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 3</h1>
+                <h2 class="subtitle">Which of the following characteristics  fits your personality the best?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Perfectionist
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Keep cool-headed in any situation
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Arrogant
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Determined – don’t ever give up,
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 4</h1>
+                <h2 class="subtitle">What is your favorite animal?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Hawk
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Dragon
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Tiger
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Frog
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 5</h1>
+                <h2 class="subtitle">What is your usual face expression?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Neutral
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    You Frown All The Time
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    You Hardly Smile
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    You Smile Alot
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 6</h1>
+                <h2 class="subtitle">When in battle, which weapon would you use?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Katana
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Senbon and Shuriken
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fists
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Kunai
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 7</h1>
+                <h2 class="subtitle">Favourite Color?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Purple/Indigo
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Blue
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Brown/Green
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Orange
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 8</h1>
+                <h2 class="subtitle">Favourite Fighting Style?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Genjutsu
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Ninjutsu
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Taijutsu
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    The Power Of Youth!!!
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 9</h1>
+                <h2 class="subtitle">Which Organization do you belong to?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Akutsuki
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Hidden Mist Anbu
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Tsuchikage's Group
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Hidden Leaf Village
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 10</h1>
+                <h2 class="subtitle">Which style do you prefer?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fire Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Earth Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Wind Style
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 11</h1>
+                <h2 class="subtitle">Whats Your Motivation?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Brother/Sister
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    My Abilities
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Proving Something
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Friends
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 12</h1>
+                <h2 class="subtitle">Choose a drink</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Tea
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Coffeee
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Soda
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 13</h1>
+                <h2 class="subtitle">Which Pokemon would you choose?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Charmander
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Squirtle 
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Bulbasaur
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Pikachu
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 14</h1>
+                <h2 class="subtitle">What Is Your Favourite Flower?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Flame Tips
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Aloe Vera
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Cactus Flowers
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Cherry Blossoms
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 15</h1>
+                <h2 class="subtitle">Lets take a look into your locker, is it</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Neat And Tidy
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Empty/Minimal
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Lots Of Pictures
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Messy
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 16</h1>
+                <h2 class="subtitle">If by some miracle you were to retire from being a ninja, what battle related ailment would you complain about?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Blindness or Really Horrible Vision
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Paralyzed From Waste Down
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Back Problems
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Carpel Tunnel
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 17</h1>
+                <h2 class="subtitle">Which style do you prefer?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fire Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Earth Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Wind Style
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 18</h1>
+                <h2 class="subtitle">Which style do you prefer?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fire Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Earth Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Wind Style
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 19</h1>
+                <h2 class="subtitle">Which style do you prefer?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fire Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Earth Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Wind Style
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container is-fluid">
+        <div class="notification">
+            <h1 class="title">Question 20</h1>
+                <h2 class="subtitle">Which style do you prefer?</h2>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Fire Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Water Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Earth Style
+                </label><br>
+                <label class="radio">
+                    <input type="radio" name="answer">
+                    Wind Style
+                </label><br><br>
+
+                <div class="field is-grouped">
+                    <p class="control">
+                        <a class="button is-link">
+                        Submit Answers
+                        </a>
+                    </p>
+                    <p class="control">
+                        <a class="button">
+                        Cancel
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
